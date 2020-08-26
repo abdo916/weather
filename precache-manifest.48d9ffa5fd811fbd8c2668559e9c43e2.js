4258f44368d4ae27c33b7c09d94659323821f4de
unknown <abdallah.gibreel@gmail.com>
2020-08-26 18:33:19 +0200

    Updates

---
+++ precache-manifest.48d9ffa5fd811fbd8c2668559e9c43e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b518eec63c60b10fef1f9c7dcc2b6b0e",
+    "url": "/github.io/weather/index.html"
+  },
+  {
+    "revision": "d41d53cc9f022f01f513",
+    "url": "/github.io/weather/static/css/main.968d2b7d.chunk.css"
+  },
+  {
+    "revision": "2c44f3aa68ff1993afc0",
+    "url": "/github.io/weather/static/js/2.2f4bfb77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/weather/static/js/2.2f4bfb77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d41d53cc9f022f01f513",
+    "url": "/github.io/weather/static/js/main.65a8bb2c.chunk.js"
+  },
+  {
+    "revision": "5fb44a06ba2bc5c3760e",
+    "url": "/github.io/weather/static/js/runtime-main.4ca75784.js"
+  }
+]);
